ee9367ff8c00ff6a550bb29d92a24832dc001bc6
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-16 18:22:52 +0000

    created a firebase app that compiles successfully with firebase and firebase database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyAJTCRVpQHBovVvJcABDK6OPa__sM6Df6Q',
+      authDomain: 'fir-demo-dc23d.firebaseapp.com',
+      databaseURL: 'https://fir-demo-dc23d.firebaseio.com',
+      projectId: 'fir-demo-dc23d',
+      storageBucket: 'fir-demo-dc23d.appspot.com',
+      messagingSenderId: '595897647490',
+      appId: '1:595897647490:web:ebc687feff5087df10cf9b',
+      measurementId: 'G-635CVDXGQ'
+    }
+  };
+  
 
 /*
  * For easier debugging in development mode, you can import the following file
