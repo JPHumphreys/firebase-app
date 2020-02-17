e1affe64aca2e4ddf7473d76d12fd3e014134e65
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-17 14:21:14 +0000

    created update and delete methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,19 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  
+export class AppComponent {
+
   course$;
   courses$;
   author$;
 
-  constructor(db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase) {
     this.courses$ = db.list('/courses').valueChanges();
     this.course$ = db.object('/courses/1').valueChanges();
     this.author$ = db.object('/authors/1').valueChanges();
   }
 
-  add(course: HTMLInputElement){
+  add(course: HTMLInputElement) {
 
     firebase.database().ref('/courses').push({
       name: course.value,
@@ -33,7 +33,24 @@ export class AppComponent{
       ]
     });
     course.value = '';
+  }
 
+  update(course) {
+
+
+
+   // firebase.database().ref('/courses/' + 0)
+   // .set(course + ' UPDATED');
+
+    // set = replaces the value
+    //
+  }
+
+  delete(course){
+    firebase.database().ref("/courses/" + course.$key)
+    .remove()
+    .then()
+    .catch(err => console.log(err));
   }
 
 
